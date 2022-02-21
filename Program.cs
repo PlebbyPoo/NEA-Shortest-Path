12093351eb56a4900251049dab424554c6849610
PooBen <rjprolad@gmail.com>
2022-02-21 16:00:42 +0000

    Creating Graph1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NEAProject
+{
+    public class Program 
+    {
+        public static void Main(string[] args)
+        {
+            double pInfinity = double.PositiveInfinity;
+            double[][] Graph1 =
+                {
+            new double[]{0,2,4, pInfinity, pInfinity },
+            new double[]{2,0,1,5, pInfinity },
+            new double[]{4,1,0,3,1 },
+            new double[]{pInfinity,5,3,0,pInfinity },
+            new double[]{pInfinity,pInfinity,1,pInfinity,0 } };
+            Console.WriteLine(Graph1);
+        }
+    }
+    
+}
+
+        
